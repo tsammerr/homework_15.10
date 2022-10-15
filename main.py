3da5a7fb3cd528ccf39d30274dd04e6a6f3f0020
main <tarasovych.maria@gmail.com>
2022-10-15 12:33:45 +0300

    init: 3 ex fix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-start = int(input('start -> '))
-end = int(input('end -> '))
-for i in range(1, 11):
-    for j in range(start, end+1):
-        print(f'{j} * {i} = {i*j}', end = '\t\t')
-    print()
+try:
+    start = int(input('start -> '))
+    end = int(input('end -> '))
+    for i in range(1, 11):
+        for j in range(start, end + 1):
+            print(f'{j} * {i} = {i * j}', end='\t\t\t')
+        print()
+except Exception as ex:
+    print(ex)
